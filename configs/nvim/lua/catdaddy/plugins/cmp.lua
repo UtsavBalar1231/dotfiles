b8e33e91d9e866eca49d7cb83c9e7df1f071db9e
UtsavBalar1231 <utsavbalar1231@gmail.com>
2024-12-30 11:16:47 +0530

    dotfiles: nvim cmp cleanup
    Change-Id: I5c8c4368abb7bbaec12a2b86310e061bf58a2883
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- configs/nvim/lua/catdaddy/plugins/cmp.lua
+++ configs/nvim/lua/catdaddy/plugins/cmp.lua
@@ -1,3 +1,58 @@
+local has_words_before = function()
+	unpack = unpack or table.unpack
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local function get_icon_provider()
+	local mini_icons_avail, mini_icons = pcall(require, "mini.icons")
+	if mini_icons_avail then
+		return function(kind)
+			return mini_icons.get("lsp", kind or "")
+		end
+	end
+	local lspkind_avail, lspkind = pcall(require, "lspkind")
+	if lspkind_avail then
+		require("lspkind").init({
+			mode = "symbol",
+			symbol_map = {
+				Codeium = "{…}",
+				Copilot = "",
+			},
+		})
+		return function(kind)
+			return lspkind.symbolic(kind, { mode = "symbol" })
+		end
+	end
+end
+
+---@type function|false|nil
+local icon_provider = false
+
+local function get_icon(ctx)
+	ctx.kind_hl_group = "BlinkCmpKind" .. ctx.kind
+	if ctx.item.source_name == "LSP" then
+		local item_doc, color_item = ctx.item.documentation, nil
+		if item_doc then
+			local highlight_colors_avail, highlight_colors = pcall(require, "nvim-highlight-colors")
+			color_item = highlight_colors_avail and highlight_colors.format(item_doc, { kind = ctx.kind })
+		end
+		if icon_provider == false then
+			icon_provider = get_icon_provider()
+		end
+		if icon_provider then
+			local icon = icon_provider(ctx.kind)
+			if icon then
+				ctx.kind_icon = icon
+			end
+		end
+		if color_item and color_item.abbr and color_item.abbr_hl_group then
+			ctx.kind_icon, ctx.kind_hl_group = color_item.abbr, color_item.abbr_hl_group
+		end
+	end
+	return ctx
+end
+
 return {
 	"saghen/blink.cmp",
 	version = "*",
@@ -8,6 +63,7 @@ return {
 		"L3MON4D3/LuaSnip",
 		"giuxtaposition/blink-cmp-copilot",
 		"onsails/lspkind.nvim",
+		"brenoprata10/nvim-highlight-colors",
 	},
 
 	opts = {
@@ -26,6 +82,12 @@ return {
 			end,
 		},
 		sources = {
+			min_keyword_length = function(ctx)
+				if ctx.mode == "cmdline" and string.find(ctx.line, " ") == nil then
+					return 2
+				end
+				return 0
+			end,
 			default = { "lsp", "path", "snippets", "luasnip", "buffer" },
 			providers = {
 				buffer = {
@@ -64,7 +126,10 @@ return {
 				},
 				snippets = {
 					name = "snippets",
-					enabled = true,
+					enabled = function(ctx)
+						return ctx ~= nil
+							and ctx.trigger.kind == vim.lsp.protocol.CompletionTriggerKind.TriggerCharacter
+					end,
 					module = "blink.cmp.sources.snippets",
 					score_offset = 850,
 					max_items = 5,
@@ -78,16 +143,24 @@ return {
 
 			["<Tab>"] = {
 				function(cmp)
-					return cmp.select_next()
+					if cmp.is_visible() then
+						return cmp.select_next()
+					elseif cmp.snippet_active({ direction = 1 }) then
+						return cmp.snippet_forward()
+					elseif has_words_before() then
+						return cmp.show()
+					end
 				end,
-				"snippet_forward",
 				"fallback",
 			},
 			["<S-Tab>"] = {
 				function(cmp)
-					return cmp.select_prev()
+					if cmp.is_visible() then
+						return cmp.select_prev()
+					elseif cmp.snippet_active({ direction = -1 }) then
+						return cmp.snippet_backward()
+					end
 				end,
-				"snippet_backward",
 				"fallback",
 			},
 
@@ -100,7 +173,7 @@ return {
 		},
 		signature = { enabled = true },
 		completion = {
-			accept = { auto_brackets = { enabled = false } },
+			accept = { auto_brackets = { enabled = true } },
 			list = {
 				selection = function(ctx)
 					if ctx.mode == "cmdline" then
@@ -133,56 +206,25 @@ return {
 					},
 					components = {
 						kind_icon = {
-							ellipsis = false,
 							text = function(ctx)
-								return require("lspkind").symbolic(ctx.kind, {
-									mode = "symbol",
-								})
+								get_icon(ctx)
+								return ctx.kind_icon .. ctx.icon_gap
 							end,
 							highlight = function(ctx)
-								return "BlinkCmpKind" .. ctx.kind
+								return get_icon(ctx).kind_hl_group
 							end,
-						},
-						label = {
-							width = {
-								fill = true,
-								max = 60,
-							},
-							text = function(ctx)
-								return ctx.label .. ctx.label_detail
-							end,
-							highlight = function(ctx)
-								local highlights = {
-									{
-										0,
-										#ctx.label,
-										group = ctx.deprecated and "BlinkCmpLabelDeprecated" or "BlinkCmpLabel",
-									},
-								}
-								if ctx.label_detail then
-									table.insert(highlights, {
-										#ctx.label,
-										#ctx.label + #ctx.label_detail,
-										group = "BlinkCmpLabelDetail",
-									})
-								end
-
-								for _, idx in ipairs(ctx.label_matched_indices) do
-									table.insert(highlights, { idx, idx + 1, group = "BlinkCmpLabelMatch" })
-								end
-
-								return highlights
-							end,
-						},
-						label_description = {
-							width = { fill = true },
-							highlight = "BlinkCmpLabelDescription",
 						},
 					},
 				},
 			},
 		},
-		appearance = { nerd_font_variant = "mono" },
+		appearance = {
+			nerd_font_variant = "mono",
+			use_nvim_cmp_as_default = false,
+		},
 	},
-	opts_extend = { "sources.default" },
+	opts_extend = {
+		"sources.cmdline",
+		"sources.default",
+	},
 }
--- configs/nvim/lua/catdaddy/plugins/lspkind.lua
+++ configs/nvim/lua/catdaddy/plugins/lspkind.lua
@@ -1,12 +1,4 @@
 return {
 	"onsails/lspkind.nvim",
 	event = "InsertEnter",
-	opts = {
-		require("lspkind").init({
-			symbol_map = {
-				Codeium = "{…}",
-				Copilot = "",
-			},
-		}),
-	},
 }
